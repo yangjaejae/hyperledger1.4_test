33777eb3272dc952b9658f6b8960f06da798b721
jaehoscott <scott.yang@chainrefund.com>
2019-11-20 16:57:49 +0900

    add Dockerfile

--- send_transaction/transaction.js
+++ send_transaction/transaction.js
@@ -92,7 +92,7 @@ async function query_result(req){
         // Evaluate the specified transaction.
         // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
         // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-        const result = await contract.evaluateTransaction('query', 'c');
+        const result = await contract.evaluateTransaction('query', 'a');
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
     } catch (error) {
