7e2d6b76f4230f3e992114cf20013c2b220c0f85
jaehoscott <scott.yang@chainrefund.com>
2019-11-20 12:54:00 +0900

    docker-compose

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DOCKER_CONTAINER_NAME="travelre.api.eos"
-DOCKER_TRAVELRE_EOS_API_IMAGE_NAME="travelre.api.eos/nodejs"
-DOCKER_TRAVELRE_EOS_API_IMAGE_TAG="v1.0.0"
+DOCKER_CONTAINER_NAME="taxrefund.api.blockchain"
+DOCKER_IMAGE_NAME="taxrefund.api.blockchain/nodejs"
+DOCKER_IMAGE_TAG="v1.0.0"
 COMPOSE_FILE=docker-compose.yaml
-echo "start TRAVELRE_EOS_API Server"
+echo "start TAXREFUND.API.BLOCKCHAIN Server"
 
 
 function restartContainers() {
@@ -12,16 +12,16 @@ function restartContainers() {
 }
 
 function rebuildDocker() {
-    echo "Stop and Remove current TRAVELRE_EOS_API Server docker container..."
+    echo "Stop and Remove current TAXREFUND.API.BLOCKCHAIN Server docker container..."
     docker rm -f $(docker ps -aqf name="${DOCKER_CONTAINER_NAME}")
 
-    if [[ "$(docker images -q ${DOCKER_TRAVELRE_EOS_API_IMAGE_NAME}:${DOCKER_TRAVELRE_EOS_API_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
-        echo "Remove TRAVELRE_EOS_API Server docker IMAGE!!.."
-        docker rmi -f ${DOCKER_TRAVELRE_EOS_API_IMAGE_NAME}:${DOCKER_TRAVELRE_EOS_API_IMAGE_TAG}
+    if [[ "$(docker images -q ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} 2> /dev/null)" != "" ]]; then
+        echo "Remove TAXREFUND.API.BLOCKCHAIN Server docker IMAGE!!.."
+        docker rmi -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
     fi
 
-    echo "Build TRAVELRE_EOS_API Server image.."
-    docker build -t ${DOCKER_TRAVELRE_EOS_API_IMAGE_NAME}:${DOCKER_TRAVELRE_EOS_API_IMAGE_TAG} .
+    echo "Build TAXREFUND.API.BLOCKCHAIN Server image.."
+    docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
     docker-compose -f ${COMPOSE_FILE} up -d 2>&1
 }
 
@@ -30,11 +30,11 @@ function askProceed () {
   read -p "Remove CA-Registry Server images and container to rebuild those items(Y) or just restart container(N) ? [Y/N] " ans
   case "$ans" in
     y|Y|"" )
-      echo "Remove and rebuild docker container of TRAVELRE_EOS_API Server"
+      echo "Remove and rebuild docker container of TAXREFUND.API.BLOCKCHAIN Server"
       rebuildDocker
     ;;
     n|N )
-      echo "Restart docker container of TRAVELRE_EOS_API Server..."
+      echo "Restart docker container of TAXREFUND.API.BLOCKCHAIN Server..."
       restartContainers
     ;;
     * )
